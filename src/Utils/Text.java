a7297a75ba2fe7d896618273e64fb1f2ca98035f
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2022-03-05 14:35:58 -0500

    Removed unnecessary files from the old data structure

---
+++ src/Utils/Text.java
@@ -0,0 +1,27 @@
+package Utils;
+
+public class Text extends Token {
+
+    protected int style;
+
+    // Constructors
+    public Text(String name) {
+        super(name);
+        this.type = "text";
+    }
+    public Text(String name, int style) {
+        super(name);
+        this.type = "text";
+        this.style = style;
+    }
+
+    // Getters
+    public int getStyle() {
+        return this.style;
+    }
+
+    // Setters
+    public void setStyle(int style) {
+        this.style = style;
+    }
+}
