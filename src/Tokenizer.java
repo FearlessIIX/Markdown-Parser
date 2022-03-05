4e90004e4b605348c254db15374bcbf6223ce74a
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2022-03-05 01:34:04 -0500

    Finished Tokenizer Version 1

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -6,8 +6,54 @@ public class Tokenizer {
     public static ArrayList<ArrayList<Token>> tokenize(ArrayList<String> lines) {
         ArrayList<ArrayList<Token>> tok_lines = new ArrayList<>();
 
+        StringBuilder tok_builder = new StringBuilder();
 
+        for (String line : lines) {
+
+            ArrayList<Token> tok_line = new ArrayList<>();
+
+            for (int i = 0; i < line.length(); i++) {
+                char ch = line.charAt(i);
+
+                // If user is using an escape character, and there is a next token
+                if (ch == '\\') {
+
+                    if (i + 1 < line.length())
+                        tok_builder.append(line.charAt(i++));
+                }
+                else if (isMetaChar(ch)) {
+                    if (!(tok_builder.isEmpty() ||
+                            tok_builder.toString().replace(" ", "").isEmpty())) {
+                        tok_line.add(new Token(tok_builder.toString()));
+                        tok_builder.delete(0, tok_builder.length());
+                    }
+
+                    tok_line.add(new Token(String.valueOf(ch)));
+                }
+                else
+                    tok_builder.append(ch);
+            }
+
+            if (!(tok_builder.isEmpty() ||
+                    tok_builder.toString().replace(" ", "").isEmpty())) {
+                tok_line.add(new Token(tok_builder.toString()));
+                tok_builder.delete(0, tok_builder.length());
+            }
+
+            tok_lines.add(tok_line);
+        }
 
         return tok_lines;
+    }
+
+    public static boolean isMetaChar(char ch) {
+        switch (ch) {
+            case '#':
+            case '_':
+            case '*':
+            case '^':
+                return true;
+        }
+        return false;
     }
 }
