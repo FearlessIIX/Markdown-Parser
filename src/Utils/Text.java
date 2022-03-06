44b11c34b0c837ec57e30b7a7eb60a7505127395
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2022-03-05 19:56:28 -0500

    Added comments

--- src/Utils/Text.java
+++ src/Utils/Text.java
@@ -5,10 +5,17 @@ public class Text extends Token {
     protected int style;
 
     // Constructors
+    /**
+     * For text, 1: plain, 2: italics, 3: bold, 4: italics and bold
+     */
     public Text(String name) {
         super(name);
         this.type = "text";
+        this.style = 1;
     }
+    /**
+     * For text, 1: plain, 2: italics, 3: bold, 4: italics and bold
+     */
     public Text(String name, int style) {
         super(name);
         this.type = "text";
